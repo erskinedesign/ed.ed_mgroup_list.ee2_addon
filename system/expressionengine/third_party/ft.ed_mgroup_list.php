20102600a53eff669c3a61e07893bc4e32e0a0f6
Wil Linssen <wil@erskinedesign.com>
2012-01-09 14:47:01 +0000

    fixed incorrectly saving data added matrix cell support

--- system/expressionengine/third_party/ft.ed_mgroup_list.php
+++ system/expressionengine/third_party/ft.ed_mgroup_list.php
@@ -4,7 +4,7 @@
  * ED Member Group List field type
  *
  * @package     ED_Mgroup_list
- * @version     1.0.0
+ * @version     1.1.0
  * @author      Wil Linssen (Erskine Design)
  * @copyright   Copyright (c) 2012 Erskine Design
  * @license     http://creativecommons.org/licenses/by-sa/3.0/ Attribution-Share Alike 3.0 Unported
@@ -20,7 +20,7 @@ class ED_mgroup_list_ft extends EE_Fieldtype
      */
     public $info = array(
         'name' => 'ED Member Group List',
-        'version' => '1.0.0'
+        'version' => '1.1.0'
     );
 
     /**
@@ -57,6 +57,30 @@ class ED_mgroup_list_ft extends EE_Fieldtype
      */
     public function display_field($data)
     {
+        $this->_get_member_groups();
+        return form_dropdown($this->field_name, $this->_member_groups, $data);
+    }
+
+    /**
+     * Display cell (for Matrix)
+     *
+     * @access  public
+     * @param   $data Current field data
+     */
+    public function display_cell($data)
+    {
+        $this->_get_member_groups();
+        return form_dropdown($this->cell_name, $this->_member_groups, $data);
+    }
+
+
+    /**
+     * Get member groups
+     *
+     * @access private
+     */
+    private function _get_member_groups()
+    {
         // Get EE instance
         $this->_EE =& get_instance();
 
@@ -72,7 +96,6 @@ class ED_mgroup_list_ft extends EE_Fieldtype
             $this->_member_groups[$row['group_id']] = $row['group_title'];
         }
 
-        return form_dropdown($this->field_name, $this->_member_groups);
     }
 
 }
